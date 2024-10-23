c603cbfd5328db9c3f3bd2b1aefa42dd59bc1bc8
guga <guga@gmail.com>
2024-10-23 17:29:28 +0800

    use google code style

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
+
 #include "root_finding.h"
 
-const  double EPSILON  = 1e-6;
+const double EPSILON = 1e-6;
 
 double example_function(double x);
 
-int main()
-{
+int main() {
     printf("%f\n", bisection(example_function, 1.5, 2, EPSILON));
 
     return 0;
 }
 
-double example_function(double x)
-{
+double example_function(double x) {
     return pow(x, 3) - pow(x, 2) - x - 1;
 }
--- root_finding.c
+++ root_finding.c
@@ -1,30 +1,23 @@
 #include "root_finding.h"
 
-double bisection(double (*func)(double), double a, double b, double tol)
-{
+double bisection(double (*func)(double), double a, double b, double tol) {
     double fa = func(a);
     double fb = func(b);
-    if (fa * fb >= 0)
-    {
+
+    if (fa * fb >= 0) {
         return DBL_MAX;
     }
 
     double c, fc;
-    while (fabs((a - b) / 2) >= tol)
-    {
+    while (fabs((a - b) / 2) >= tol) {
         c = (a + b) / 2;
         fc = func(c);
 
-        if (fa * fc < 0)
-        {
+        if (fa * fc < 0) {
             b = c;
-        }
-        else if (fa * fc > 0)
-        {
+        } else if (fa * fc > 0) {
             a = c;
-        }
-        else
-        {
+        } else {
             return c;
         }
     }
