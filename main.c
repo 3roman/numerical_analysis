e940007fd689a0a6be46ad41aa116bd862399ead
guga <guga@gmail.com>
2024-10-23 16:47:27 +0800

    define the iteration tolerance

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <math.h>
 #include "root_finding.h"
+
+const  double EPSILON  = 1e-6;
 
 double example_function(double x);
 
 int main()
 {
-    printf("%f\n", bisection(example_function, 1.5, 2, 1e-6));
+    printf("%f\n", bisection(example_function, 1.5, 2, EPSILON));
 
     return 0;
 }
